a602f98e00604c117ff462ce89acbd2abfea5502
Owen Batista Roman <48600146+OwenBatista@users.noreply.github.com>
2024-12-17 17:09:06 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -26,18 +26,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         score.increase_score()
 
+    #detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
+    #detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
